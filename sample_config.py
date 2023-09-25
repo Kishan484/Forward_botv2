e6725e7ee28309f03f9c5b79f3aacd7faf72603e
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2023-09-25 11:11:08 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    APP_ID = 000
-    API_HASH = "000"
-    BOT_TOKEN = "000"
+    APP_ID = "28103752"
+    API_HASH = "479579601469809b9a9c8ffc15fbe11a"
+    BOT_TOKEN = "6527624487:AAHshEYDIebjftL_pUWMvwgsMIc3vHmlPbs"
     BOT_WORKERS = 40
     SUDO_USERS = "000,111,222,333"
-    DB_URI = "mongo...."
+    DB_URI = "mongodb+srv://Kishan484:Kishan484@cluster0.xhxy0uu.mongodb.net/?retryWrites=true&w=majority"
     SESSION_NAME = "test"
-    MULTI_USER_MODE = False
+    MULTI_USER_MODE = True
